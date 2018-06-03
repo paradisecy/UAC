5c295e141b1f7bb236946240963ec85008764019
DESKTOP-CF191QO\Andreas <andriou.p@live.com>
2018-06-04 01:15:35 +0300

    init

---
+++ ApplicationLoaderService/ExecuteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Http;
+
+
+namespace Toolkit
+{
+    public class ExecuteController : ApiController
+    {
+       [HttpGet]
+        public void Execute(string command)
+        {
+            string applicationName = $"cmd.exe {command}";
+
+            ApplicationLoader.PROCESS_INFORMATION procInfo;
+            ApplicationLoader.StartProcessAndBypassUAC(applicationName, out procInfo);
+        }
+    }
+}
---
+++ ApplicationLoaderService/LoaderService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace Toolkit
+{
+    public partial class LoaderService : ServiceBase
+    {
+        protected override void OnStart(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8081");
+
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+            server.OpenAsync().Wait();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
